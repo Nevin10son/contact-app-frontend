1c1246f8588e3dea0018aa3c0003e118965363bc
Nevin10son <nevintenson235@gmail.com>
2024-06-13 06:46:19 +0530

    Search Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddContacts from './components/AddContacts';
+import SearchContact from './components/SearchContact';
 
 function App() {
   return (
     <div className="App">
       <AddContacts/>
+      <SearchContact/>
     </div>
   );
 }
