e66b46871c882b38e5755dd019ffab7dcc530303
Nevin10son <nevintenson235@gmail.com>
2024-06-13 06:55:53 +0530

    viewall complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddContacts from './components/AddContacts';
 import SearchContact from './components/SearchContact';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div className="App">
       <AddContacts/>
       <SearchContact/>
+      <ViewAll/>
     </div>
   );
 }
